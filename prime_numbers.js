e69a0267fe815dcace65f4101de61c0bc5ab3623
DMayanja21 <you@example.com>
2019-06-19 10:18:26 +0300

    edit

---
+++ prime_numbers.js
@@ -0,0 +1,13 @@
+function isPrime(n){
+	if(n<2) return false;
+	for(var m=2;m<n;m++){
+		if(n%m==0) return false;
+	}
+	return true;
+}
+var primes=[];
+for(m=0;m<101;m++){
+	if(isPrime(m))
+		primes.push(m);
+}
+console.log(primes);

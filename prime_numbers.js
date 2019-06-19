ebedb0b0b6eca60a2094cffddd663d6abd957832
DMayanja21 <you@example.com>
2019-06-19 11:59:19 +0300

    edits

--- prime_numbers.js
+++ prime_numbers.js
@@ -1,13 +1,13 @@
-function isPrime(n){
-	if(n<2) return false;
-	for(var m=2;m<n;m++){
-		if(n%m==0) return false;
+function isPrime(n){//generate a function that can identify prime numbers (numbers with only two factors 1 and itself)
+	if(n<2) return false;//any values less tean 2 will bring a false result (0 and 1 in most cases aren't considered prime numbers)
+	for(var m=2;m<n;m++){//generate a loop from which possible facotrs can be derived, identified as m
+		if(n%m==0) return false;//if the selected value n when divided by our possible factors m gives no remainder, then m is a factor of n and thus n cannot be a prime number
 	}
-	return true;
+	return true;//when m found not to be a factor of n, the selected value of n is a prime number
 }
-var primes=[];
-for(m=0;m<101;m++){
+var primes=[];//generates an array to be defined by the subsequent code
+for(m=0;m<==100;m++){//generates a loop within which values of m that fall within the given range are ran through the function generated above
 	if(isPrime(m)==true)
-		primes.push(m);
+		primes.push(m);//values of m found to be "true" are then pushed to the array
 }
-console.log(primes);
+console.log(primes);//retuns values for the selected array 
